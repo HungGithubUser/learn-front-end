683781b7ba5ff614ff50527b4b70903579133692
Hung Duong <hung.duong@ansarada.com>
2023-06-11 09:18:47 +0700

    rename refactoring

--- js/constants.css.js
+++ js/constants.css.js
@@ -1,2 +1,2 @@
 export const CURSOR_POINTER_CSS_CLASS = "cursor-pointer"
-export const CLICKABLE_FONT_AWESOME_ICON_CSS_CLASS = "clickable-font-awesome"
+export const CLICKABLE_FONT_AWESOME_ICON_CSS_CLASS = "pointer-events-none"
