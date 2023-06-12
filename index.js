4206eba99187f82a33684fd570c743f218050170
Hung Duong <hung.duong@ansarada.com>
2023-06-12 09:12:47 +0700

    fix library

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { DIV_TAG_NAME } from "./js/constants.dom.js";
 import { TodoList } from "./js/todolist.js";
 import { TodoListViewBuilder } from "./js/todolist.viewbuilder.js";
+
 let containerDivElement = getNewDivElement("container-fluid")
 let rowDivElement = getNewDivElement("row justify-content-center")
 let todoListView = new TodoListViewBuilder(new TodoList()).withInputField().withOuterDivElementCssClass("col-6").withCompleteTaskToggleOnItemClick().build()
